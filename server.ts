552af76062ad5faf6d89160baa94913ed92bc5fb
Oscar Pérez <mr.omiguelperez@gmail.com>
2022-03-23 19:09:50 -0500

    add compressor

--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ import * as express from 'express';
 import {join} from 'path';
 
 // Express server
-export const app = express();
+export const app = express.default();
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
